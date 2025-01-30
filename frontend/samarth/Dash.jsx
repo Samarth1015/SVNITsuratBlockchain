204ad776e9cf583a63a3e5814f03f2f56857af6c
samarth <202351125@iiitvadodara.ac.in>
2025-01-30 23:59:48 +0530

    . is the key for success in life

--- frontend/samarth/Dash.jsx
+++ frontend/samarth/Dash.jsx
@@ -9,6 +9,7 @@ import { ExtractDataFromPara } from "@/app/api/InsertData/ExtractDataFromInsert"
 import Databases from "./component/Databases";
 import { generateRandom } from "../utils/generateRandom";
 import Image from "next/image";
+import { set } from "rsuite/esm/internals/utils/date";
 const uri = "mongodb://localhost:27017/";
 
 const ChatGPTInterface = () => {
@@ -22,6 +23,7 @@ const ChatGPTInterface = () => {
   const [anotherData, setAnotherData] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
   const [loading, setLoading] = useState(false);
+  const [workinguri, setWorkingUri] = useState(`mongodb://localhost:27017/`);
   const [nameOfOwner, setNameOfOwner] = useState("");
   const [readDataOperation, setReadDataOperation] = useState({
     response: [],
@@ -70,7 +72,7 @@ const ChatGPTInterface = () => {
   //function to handle send
   const handleSend = async () => {
     // setMessages(input)
-    const uri = "mongodb://localhost:27017/";
+    const uri = workinguri;
     const data = {
       paragraph: input,
     };
@@ -344,7 +346,8 @@ const ChatGPTInterface = () => {
     <div
       className={`flex flex-row h-screen ${
         isDarkMode ? "bg-black text-white" : "bg-gray-100 text-gray-900"
-      }`}>
+      }`}
+    >
       {/* <div>
         <button
           onClick={async () => {
@@ -381,46 +384,60 @@ const ChatGPTInterface = () => {
       <div className="flex flex-col justify-between ">
         <div className="flex flex-row h-full">
           <div className="w-fit border-r-2 border-[#292929]  px-4">
-            <h1 className="font-semibold text-[#e6e0e0] text-center my-5">
-              All Databases
+            <h1
+              className={`font-semibold text-[#e6e0e0] text-center my-5 ${
+                workinguri === "mongodb://localhost:27017/"
+                  ? "bg-[#292929]"
+                  : ""
+              } `}
+              onClick={() => {
+                setWorkingUri("mongodb://localhost:27017/");
+              }}
+            >
+              local host Database
             </h1>
 
-            {dbs &&
-              dbs.map((e, index) => <Databases key={index} name={e.name} />)}
-
-            {/* <ul className="gap-2 flex flex-col">
-          {allDatabases && allDatabases.length > 0 ? (
-            allDatabases.map((db, index) => (
-              <li
-                className="w-full px-5 py-2 text-sm bg-[#292929] rounded-2xl text-center hover:bg-[#626262] hover:scale-105 transition-all duration-100"
-                key={index}
-              >
-                {db["name"]}
-              </li>
-            ))
-          ) : (
-            <p>No databases found.</p>
-          )}
-        </ul> */}
+            <ul className="gap-2 flex flex-col">
+              {allDatabases && allDatabases.length > 0 ? (
+                allDatabases.map((db, index) => (
+                  <li
+                    className="w-full px-5 py-2 text-sm bg-[#292929] rounded-2xl text-center hover:bg-[#626262] hover:scale-105 transition-all duration-100"
+                    key={index}
+                  >
+                    {db["name"]}
+                  </li>
+                ))
+              ) : (
+                <p>No databases found.</p>
+              )}
+            </ul>
           </div>
           <div className="w-fit border-r-2 border-[#292929]  px-4">
-            <h1 className="font-semibold text-[#e6e0e0] text-center my-5">
-              All Collection
+            <h1
+              className={`font-semibold text-[#e6e0e0] text-center my-5 ${
+                workinguri != "mongodb://localhost:27017/" ? "bg-[#292929]" : ""
+              }
+              }`}
+              onClick={() => {
+                setWorkingUri();
+              }}
+            >
+              another database
             </h1>
-            {/* <ul className="gap-2 flex flex-col">
-          {anotherData && anotherData.length > 0 ? (
-            anotherData.map((db, index) => (
-              <li
-                className="w-full px-5 py-2 text-sm bg-[#292929] rounded-2xl text-center hover:bg-[#626262] hover:scale-105 transition-all duration-100"
-                key={index}
-              >
-                {db["name"]}
-              </li>
-            ))
-          ) : (
-            <p>No databases found.</p>
-          )}
-        </ul> */}
+            <ul className="gap-2 flex flex-col">
+              {anotherData && anotherData.length > 0 ? (
+                anotherData.map((db, index) => (
+                  <li
+                    className="w-full px-5 py-2 text-sm bg-[#292929] rounded-2xl text-center hover:bg-[#626262] hover:scale-105 transition-all duration-100"
+                    key={index}
+                  >
+                    {db["name"]}
+                  </li>
+                ))
+              ) : (
+                <p>No databases found.</p>
+              )}
+            </ul>
           </div>
         </div>
 
@@ -460,6 +477,7 @@ const ChatGPTInterface = () => {
                   signature
                 );
                 const res = await contract.viewConnectionString(add);
+                setWorkingUri(res);
                 console.log("Contract Response: ", res);
 
                 // Call the Next.js API to connect to MongoDB
@@ -490,7 +508,8 @@ const ChatGPTInterface = () => {
                 console.error("Error:", error.message);
               }
             }}
-            className="px-4 w-full py-2 rounded-lg cs">
+            className="px-4 w-full py-2 rounded-lg cs"
+          >
             Connect
           </button>
         </div>
@@ -503,18 +522,22 @@ const ChatGPTInterface = () => {
             readDataOperation.response.map((obj, index) => (
               <div
                 key={index}
-                className="p-4 border rounded-lg bg-[#292929] text-white">
+                className="p-4 border rounded-lg bg-[#292929] text-white"
+              >
                 <pre>{JSON.stringify(obj, null, 2)}</pre>
               </div>
             ))}
           {generalOperation.flag && (
             <>
-              <p className="text-green-500 text-6xl font-bold self-center">{generalOperation.response}</p>
+              <p className="text-green-500 text-6xl font-bold self-center">
+                {generalOperation.response}
+              </p>
             </>
           )}
           {loading && (
             <>
-              <img className="self-center opacity-80"
+              <img
+                className="self-center opacity-80"
                 src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXlwdG4xYXExMmNuOWFpc2hveWdrMnZqMzdmMjhoNG50M2ZtdWE0NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/tAjb5pyCEBhEb8jWxC/giphy.gif"
                 height="200"
                 width="250"
@@ -528,7 +551,8 @@ const ChatGPTInterface = () => {
         <div
           className={`p-4 border-t ${
             isDarkMode ? "border-[#292929] " : "border-gray-300 bg-white"
-          }`}>
+          }`}
+        >
           <div className="w-full flex items-center space-x-3 ">
             <div className="card w-full hover:p-1">
               <input
@@ -552,7 +576,8 @@ const ChatGPTInterface = () => {
                   handleSend();
                 }
               }}
-              onClick={handleSend}>
+              onClick={handleSend}
+            >
               Send
             </button>
           </div>
